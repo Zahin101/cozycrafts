55f2bf07b2ed74b63e18d92b6d58708eb9ceb38b
Zahin101 <zahin.rydha@g.bracu.ac.bd>
2022-08-23 21:22:24 +0600

    shipping details confirm order order details

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const errorMiddleWare = require("./middleware/error");
 const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+
+//config
+
+dotenv.config({ path: "backend/config/config.env" });
 
 app.use(express.json());
 app.use(cookieParser());
@@ -10,10 +15,12 @@ app.use(cookieParser());
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoutes");
 const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
+app.use("/api/v1", payment);
 
 //MiddleWare For Errors
 
